d628bade3bf39a2ae5db2fff5e55611339966ead
Info htmlBurger <info@htmlburger.com>
2014-11-07 10:45:16 +0000

    Upload: scripts/functions.js

--- scripts/functions.js
+++ scripts/functions.js
@@ -4,39 +4,18 @@
 
 	$doc.ready(function() {
 
-		var cbClose = function() {
-			var $video = $(this);
-
-			$video.find('.sqs-video-overlay').css({
-				'opacity' : '1',
-				'visibility' : 'visible'
-			});
-		}
-
 		$('.sqs-video-wrapper').colorbox({
 			html: function() {
 				return $(this).data('html');
 			},
 			scrolling: false,
 			onComplete: function() {
-
 				var $video = $(this);
 
 				$.colorbox.resize();
 
 				$video.find('video, iframe').remove();
-	
-				$('#cboxLoadedContent iframe').load(function() {
-
-					$video.find('.sqs-video-overlay').css({
-						'opacity' : '1',
-						'visibility' : 'visible'
-					});
-					
-				});
-			},
-			onCleanup: cbClose,
-			onClosed: cbClose
+			}
 		})
 	});
 })(jQuery, window, document);
